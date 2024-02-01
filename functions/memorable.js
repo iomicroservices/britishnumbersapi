9aba30f80e5ebcedb1b39e61e4fe9e8aa6f31eed
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-01 17:43:57 +0000

    Update memorable.js

--- functions/memorable.js
+++ functions/memorable.js
@@ -1,22 +1,13 @@
-// /functions/memorable.js
 export async function onRequestGet(context) {
-    // Access environment variables using context.env
-    const baseURL = context.env.MEMORABLE_WEBHOOK_BASE_URL; // Environment variable for the base URL
-    
-    // Extract the search parameters from the request URL
+    const baseURL = context.env.MEMORABLE_WEBHOOK_BASE_URL;
     const url = new URL(context.request.url);
-    const searchParams = url.search; // This includes the '?' and the query string
-    
-    // Construct the destination URL using the base URL from environment variables and search parameters from the request
+    const searchParams = url.search;
     const destinationURL = `${baseURL}${searchParams}`;
-    
     try {
         const response = await fetch(destinationURL, {
             method: 'GET',
             headers: context.request.headers,
         });
-
-        // Forward the response from the fetch call
         return new Response(response.body, {
             status: response.status,
             statusText: response.statusText,
