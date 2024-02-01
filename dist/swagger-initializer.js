e2962629c65072b1543abfd86b7453ac2dd1973b
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-01 14:28:57 +0000

    Update swagger-initializer.js url

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://github.com/iomicroservices/britishnumbersapi/blob/e7b0b68a0ffe3c38a66c3109cb2b9804c46734b2/swagger.json",
+    url: "https://raw.githubusercontent.com/iomicroservices/britishnumbersapi/main/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
